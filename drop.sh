3d5f055be484b62bcce06568776c7dfbb1e4343c
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2023-05-12 11:09:40 +0200

    Added simple script to completly drop a postgre database

---
+++ drop.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -e
+
+pushd /
+    echo "Stopping postgresql"
+    sudo systemctl stop postgresql
+
+    echo "Removing postgresql data"
+    sudo rm -rf /var/lib/postgres/data
+    sudo mkdir /var/lib/postgres/data
+    sudo chown postgres:postgres /var/lib/postgres/data
+
+    echo "Initializing postgresql database"
+    sudo -u postgres initdb --locale=C.UTF-8 --encoding=UTF8 -D /var/lib/postgres/data --data-checksums
+    
+    echo "Starting postgresql"
+    sudo systemctl start postgresql
+
+    echo "Creating postgresql user"
+    sudo -u postgres createuser -s nicolas
+popd
